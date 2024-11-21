981196f7a62d657852b92d2948163468a50ec99f
ZeldoRetro <shadar669@gmail.com>
2024-11-20 22:57:04 -0500

    Mapping des maisons de Cocorico + Coding ennemis Lake Hylia Cave

--- data/maps/A Link To The Past/Light World/Houses/Kakariko/chickens_house.lua
+++ data/maps/A Link To The Past/Light World/Houses/Kakariko/chickens_house.lua
@@ -1,24 +1,12 @@
--- Lua script of map A Link To The Past/Light World/Houses/Kakariko/chickens_house.
--- This script is executed every time the hero enters this map.
-
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation:
--- http://www.solarus-games.org/doc/latest
-
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
-
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
+function chest_boomerang:on_opened()
+  if game:get_value("get_boomerang_1") then
+    hero:start_treasure("consumables/arrow_refill",1,"after_boomerang_arrow")
+  else
+    hero:start_treasure("inventory/boomerang",1,"get_boomerang_1")
+  end
 end
 
--- Event called after the opening transition effect of the map,
--- that is, when the player takes control of the hero.
-function map:on_opening_transition_finished()
-
-end
+function weak_door_1:on_opened() sol.audio.play_sound("secret") end
